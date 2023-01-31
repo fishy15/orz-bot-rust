540324599be8b013f54a7a92ffb24fbcef2cf158
= <aaryan.prakash3.14@gmail.com>
2023-01-31 02:57:41 -0600

    Add basic logger

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,13 @@ use serenity::client::Context;
 use serenity::framework::StandardFramework;
 use serenity::framework::standard::CommandResult;
 use serenity::framework::standard::macros::{command, group};
+use serenity::model::prelude::MessageUpdateEvent;
+use serenity::model::prelude::{ChannelId, GuildId, MessageId};
 use serenity::model::prelude::{Message, Ready};
 use serenity::prelude::{GatewayIntents, EventHandler};
+
+const LOG_CHANNEL: ChannelId = ChannelId(550190785358856223);
+const LOGGED_MESSAGES: usize = 5000;
 
 // Event Handler
 struct Handler;
@@ -16,6 +21,22 @@ struct Handler;
 impl EventHandler for Handler {
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name)
+    }
+
+    async fn message_update(&self, ctx: Context, old_msg: Option<Message>, _: Option<Message>, _: MessageUpdateEvent) {
+        if let Some(message) = old_msg {
+            let deletion_msg = format!("Message edited by `{}`", message.author.name);
+            let _ = LOG_CHANNEL.send_message(&ctx.http, |m| m.content(deletion_msg)).await;
+            let _ = LOG_CHANNEL.send_message(&ctx.http, |m| m.content(message.content)).await;
+        }
+    }
+
+    async fn message_delete(&self, ctx: Context, channel_id: ChannelId, message_id: MessageId, _: Option<GuildId>) {
+        if let Some(message) = ctx.cache.message(channel_id, message_id) {
+            let deletion_msg = format!("Message deleted by `{}`", message.author.name);
+            let _ = LOG_CHANNEL.send_message(&ctx.http, |m| m.content(deletion_msg)).await;
+            let _ = LOG_CHANNEL.send_message(&ctx.http, |m| m.content(message.content)).await;
+        }
     }
 }
 
@@ -44,6 +65,7 @@ async fn main() {
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
         .framework(framework)
+        .cache_settings(|f| f.max_messages(LOGGED_MESSAGES))
         .await
         .expect("Error creating client");
 
